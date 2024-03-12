0b3a01249d3444472c36f648b0c8673ee7d10faa
Sérgio Nunes <sagnunes@gmail.com>
2024-03-12 15:00:12 +0000

    added alt to image

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ for(let i = 0; i < score.length; i++){
     for(let t = 0; t < 5; t++){
         const starIcon = new Image();
         starIcon.src = "../images/icon-star.svg"
+        starIcon.alt = "start icon"
         score[i].appendChild(starIcon)
     }
 }
